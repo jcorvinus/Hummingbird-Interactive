7c204d568dd76af83f35064b89272dd18bf58a17
Joshua Corvinus <dude.dudeness@gmail.com>
2016-06-11 02:21:49 -0400

    Bird behavior and sound scripting

---
+++ Assets/Scripts/Bird/Bird.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bird : MonoBehaviour
+{
+
+    // defines
+    public enum BirdAIState { Sitting, Floating, FlyingToGoal }
+
+    #region Other Components
+    private SphereCollider sphereCollider;
+    private Rigidbody rigidBody;
+    [SerializeField] MeshRenderer[] renderers;
+    [SerializeField] AudioSource birdMouth;
+    [SerializeField] AudioSource wingsSource;
+    [SerializeField] AudioClip song;
+    [SerializeField] AudioClip chirp;
+    #endregion
+
+    #region State Variables
+    [SerializeField] BirdAIState currentState = BirdAIState.Sitting;
+    /// <summary>If true, bird is flying in an arc.
+    /// If false, bird is doing a quick jump to its destination.</summary>
+    bool smoothFlight = false;
+
+    #endregion
+
+    #region Debug
+    [Header("Debug Variables")]
+    public Color birdMeshSelectedColor = Color.red;
+
+    [Header("Debug Commands")]
+    public bool BirdSing = false;
+    public bool BirdChirp = false;
+    #endregion
+
+    // Use this for initialization
+    void Awake ()
+    {
+        sphereCollider = GetComponent<SphereCollider>();
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+    void OnEnable()
+    {
+        BirdManager.Instance.AddBird(this);
+    }
+
+    void OnDisable()
+    {
+        BirdManager.Instance.RemoveBird(this);
+    }
+
+    void OnDestroy()
+    {
+        BirdManager.Instance.RemoveBird(this);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        switch (currentState)
+        {
+            case BirdAIState.Sitting:
+                break;
+
+            case BirdAIState.Floating:
+                break;
+
+            case BirdAIState.FlyingToGoal:
+                if(smoothFlight)
+                {
+
+                }
+                else
+                {
+
+                }
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void Sing()
+    {
+        birdMouth.clip = song;
+        birdMouth.Play();
+    }
+
+    private void Chirp()
+    {
+        birdMouth.clip = chirp;
+        birdMouth.Play();
+    }
+
+    private void ProcessDebugCommands()
+    {
+        if(BirdSing)
+        {
+            BirdSing = false;
+        }
+
+        if(BirdChirp)
+        {
+            BirdSing = false;
+        }
+    }
+
+    void OnDeselect()
+    {
+
+    }
+
+    void OnSelect()
+    {
+        BirdManager.Instance.SelectBird(this);
+    }
+}
---
+++ Assets/Scripts/Bird/BirdManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BirdManager : MonoBehaviour
+{
+    public static BirdManager Instance;
+
+    List<Bird> birdList;
+    Bird selectedBird;
+
+    bool hasInitialized = false;
+
+    void Awake()
+    {
+        if (hasInitialized) return;
+        Instance = this;
+        birdList = new List<Bird>();
+
+        hasInitialized = true;
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void SelectBird(Bird newSelection)
+    {
+        
+
+    }
+
+    public void AddBird(Bird newBird)
+    {
+        Awake();
+        birdList.Add(newBird);
+    }
+
+    public void RemoveBird(Bird bird)
+    {
+        birdList.Remove(bird);
+    }
+}
