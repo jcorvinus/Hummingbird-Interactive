ec1924acb6cefe27cacdb3c07f5ff5f24dca8328
sean <sean.d.payne@gmail.com>
2016-06-11 02:15:17 -0400

    Add Hummingbird to SourceAssets

---
+++ SourceAssets/Hummingbird/Scripts/HummingbirdCameraScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HummingbirdCameraScript : MonoBehaviour {
+	public GameObject target;
+	public float turnSpeed=.2f;
+	public GameObject cameraOrigin;
+	float cameraAngleX=150f;
+	float cameraAngleY=0f;
+
+	public void Start(){
+		Quaternion arotation = Quaternion.identity;
+		Vector3 eua = Vector3.zero;
+		eua.y = 360f-cameraAngleY;
+		eua.z = 0f;
+		eua.x = 180f+cameraAngleX;
+		arotation.eulerAngles = eua;
+		cameraOrigin.transform.localRotation= arotation;
+	}
+	
+	public void CameraRotationX(float x){
+		cameraAngleX = x;
+		Quaternion arotation = Quaternion.identity;
+		Vector3 eua = Vector3.zero;
+		eua.y = 360f-cameraAngleY;
+		eua.z = 0f;
+		eua.x = 180f+cameraAngleX;
+		arotation.eulerAngles = eua;
+		cameraOrigin.transform.localRotation= arotation;
+	}
+	public void CameraRotationY(float y){
+		cameraAngleY = y;
+		Quaternion arotation = Quaternion.identity;
+		Vector3 eua = Vector3.zero;
+		eua.y = 360f-cameraAngleY;
+		eua.z = 0f;
+		eua.x = 180f+cameraAngleX;
+		arotation.eulerAngles = eua;
+		cameraOrigin.transform.localRotation= arotation;
+	}
+	void FixedUpdate(){
+		transform.position = Vector3.Lerp (transform.position,target.transform.position,Time.deltaTime*10f);
+		transform.rotation = Quaternion.Lerp (transform.rotation,target.transform.rotation,Time.deltaTime*turnSpeed);
+	}
+}
---
+++ SourceAssets/Hummingbird/Scripts/HummingbirdUserControllerScript.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HummingbirdUserControllerScript : MonoBehaviour {
+
+	public HummingbirdCharacterScript hummingbirdCharacter;
+
+	void Start () {
+		hummingbirdCharacter = GetComponent<HummingbirdCharacterScript> ();	
+	}
+
+	void Update(){
+		if (Input.GetKeyDown(KeyCode.A)) {
+			if (!hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.HopLeft();
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.W)) {
+			if (!hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.HopForward();
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.D)) {
+			if (!hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.HopRight();
+			}
+		}
+
+
+
+		if (Input.GetKeyDown(KeyCode.N)) {
+			if(hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.Nectar();
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.H)) {
+			if(hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.Hit();
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.R)) {
+			if(!hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.LookAround();
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.S)) {
+			if(!hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.StandFlap();
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.G)) {
+			if(!hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.StandUp();
+			}
+		}
+		if (Input.GetKeyDown(KeyCode.B)) {
+			if(!hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.SitDown();
+			}
+		}
+
+
+		if (Input.GetKeyDown(KeyCode.L)) {
+			if(hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.Landing();
+			}
+		}
+
+		if (Input.GetButtonDown ("Jump")) {
+			if (!hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.Soar ();
+			}
+		}
+
+		if (Input.GetButtonDown ("Fire1")) {
+			if(hummingbirdCharacter.isFlying){
+				hummingbirdCharacter.Attack ();
+			}
+		}
+	}
+	
+	void FixedUpdate(){
+		float v = Input.GetAxis ("Vertical");
+		float h = Input.GetAxis ("Horizontal");
+		hummingbirdCharacter.Move (v,h);		
+	}
+}
