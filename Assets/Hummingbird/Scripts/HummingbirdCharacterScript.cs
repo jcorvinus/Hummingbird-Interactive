29fda380370a8461f95ef22b8e1aec5004ba589d
Joshua Corvinus <dude.dudeness@gmail.com>
2016-06-11 02:47:45 -0400

    Importing Hummingbird asset.

---
+++ Assets/Hummingbird/Scripts/HummingbirdCharacterScript.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HummingbirdCharacterScript : MonoBehaviour {
+	public Animator hummingbirdAnimator;
+	public float animSpeed=5f;
+	Rigidbody hummingbirdRigid;
+	public bool isFlying=false;
+
+	public float forwardSpeed=3f;
+	public float hopSpeed=2f;
+	public float rotateSpeed=.2f;
+
+
+	void Start () {
+		hummingbirdAnimator = GetComponent<Animator> ();
+		hummingbirdAnimator.speed = animSpeed;
+		hummingbirdRigid = GetComponent<Rigidbody> ();
+	}	
+
+	public void AnimSpeedSet(float speed){
+		animSpeed = speed;
+		hummingbirdAnimator.speed = animSpeed;
+	}
+
+	public void ForwardSpeedSet(float speed){
+		forwardSpeed = speed;
+	}
+
+	public void Landing(){
+		hummingbirdAnimator.SetTrigger ("Landing");
+		hummingbirdRigid.useGravity = true;
+		isFlying = false;
+	}
+	
+	public void Soar(){
+		hummingbirdAnimator.SetTrigger ("Soar");
+		hummingbirdRigid.useGravity = false;
+		isFlying = true;
+	}
+
+	public void HopForward(){
+		hummingbirdAnimator.SetTrigger ("HopForward");
+		hummingbirdRigid.AddForce ((transform.forward +transform.up)* hopSpeed,ForceMode.Impulse);	
+	}
+	
+	public void HopRight(){
+		hummingbirdAnimator.SetTrigger ("HopRight");
+		hummingbirdRigid.AddForce ((transform.forward +transform.up)* hopSpeed,ForceMode.Impulse);
+		hummingbirdRigid.AddTorque (transform.up * rotateSpeed,ForceMode.Impulse);
+	}
+	
+	public void HopLeft(){
+		hummingbirdAnimator.SetTrigger ("HopLeft");
+
+		hummingbirdRigid.AddForce ((transform.forward +transform.up)* hopSpeed,ForceMode.Impulse);
+		hummingbirdRigid.AddTorque (-transform.up * rotateSpeed,ForceMode.Impulse);
+	}
+
+
+	public void Attack(){
+		hummingbirdAnimator.SetTrigger ("Attack");
+	}
+
+	public void LookAround(){
+		hummingbirdAnimator.SetTrigger ("LookAround");
+	}
+
+	public void SitDown(){
+		hummingbirdAnimator.SetTrigger ("SitDown");
+	}
+
+	public void StandUp(){
+		hummingbirdAnimator.SetTrigger ("StandUp");
+	}
+
+	public void StandFlap(){
+		hummingbirdAnimator.SetTrigger ("StandFlap");
+	}
+
+	public void Nectar(){
+		hummingbirdAnimator.SetTrigger ("Nectar");
+	}
+
+	public void Hit(){
+		hummingbirdAnimator.SetTrigger ("Hit");
+	}
+
+	public void Move(float v,float h){
+
+		if (isFlying) {
+			if (v > 0.1f || v < -0.1f) {
+				hummingbirdRigid.AddForce ((transform.forward * forwardSpeed - transform.up * .05f) * -v);
+			} else {
+				hummingbirdRigid.AddForce (transform.forward * forwardSpeed);
+			}
+			hummingbirdRigid.AddTorque (transform.up * h * rotateSpeed);
+		}
+
+
+		hummingbirdAnimator.SetFloat ("Forward",v);
+		hummingbirdAnimator.SetFloat ("Turn",h);
+	}
+}
---
+++ Assets/Hummingbird/readme.txt
@@ -0,0 +1,12 @@
+Thank you for downloading.
+
+-Modals folder contains BroadTailHummingbird.fbx.
+-Animations folder contains additional animation's files.
+-Scenes folder contains a demo scene.
+-Prefabs folder contains a prefab that was used in the demo scene.
+This prefab's Head size is larger than original fbx file's model.
+This prefab's Head's bone's scale is set 1.4.
+This can be changed to your preference.
+(Search the "Head" in the demo scene, then change the scale by inspector.)
+
+support@ryousirikigaku.com
